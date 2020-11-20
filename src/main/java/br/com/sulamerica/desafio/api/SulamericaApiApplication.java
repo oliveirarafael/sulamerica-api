1e91caede22288dfd135f2227a0d1fe46c939d60
oliveirarafael <oliveira.r.rafael@gmail.com>
2020-11-20 10:52:56 -0300

    Iniciando Projeto
    Foi iniciado o projeto do desafio SulAmerica

---
+++ src/main/java/br/com/sulamerica/desafio/api/SulamericaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sulamerica.desafio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulamericaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SulamericaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/sulamerica/desafio/api/SulamericaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sulamerica.desafio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulamericaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
