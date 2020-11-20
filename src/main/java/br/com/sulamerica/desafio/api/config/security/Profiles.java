e2fbe934b32d8965caecdebe51ff6be3c01c6ec0
oliveirarafael <oliveira.r.rafael@gmail.com>
2020-11-20 11:54:40 -0300

    Configracao Projeto
    Realizado os arquivos properties dos ambientes e cirado o arquivo para configuracao de LOG.

---
+++ src/main/java/br/com/sulamerica/desafio/api/config/security/Profiles.java
@@ -0,0 +1,8 @@
+package br.com.sulamerica.desafio.api.config.security;
+
+public class Profiles {
+    private Profiles(){}
+    public static final String DESENVOLVIMENTO = "desenvolvimento";
+    public static final String TESTE = "teste";
+    public static final String PRODUCAO = "producao";
+}
